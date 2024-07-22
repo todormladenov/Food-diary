4d791b13fc880eaee0b8c873180064c7bcab39ff
Todor Mladenov <toshko958@gmail.com>
2024-07-22 21:19:18 +0200

    Add Header Component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,37 @@
+import { Link } from 'react-router-dom';
+import logo from '../../assets/3d-old-text-2-1-1024x1024.png';
+import './Header.css';
+
+export default function Header() {
+
+    return (
+        <>
+            <div className="header">
+                <div className="logo">
+                    <img src={logo} alt="Food Diary" />
+                </div>
+                <div className="nav">
+                    <ul>
+                        <li>
+                            <Link className="link" to='/'>HOME</Link>
+                        </li>
+                        <li>
+                            <Link className="link active" to="/diary/10 09 2024" >DIARY</Link>
+                        </li>
+                        <li>
+                            <Link className="link" to="/create-food">CREATE FOOD</Link>
+                        </li>
+                        <li>
+                            <Link className="link" to="/calculator">CALCULATOR</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="user-info">
+                    <span>Hi, Username</span>
+                    <span>Goals</span>
+                    <span>Log Out</span>
+                </div>
+            </div >
+        </>
+    );
+}
