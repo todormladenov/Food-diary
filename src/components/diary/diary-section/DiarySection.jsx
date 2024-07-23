9ffba0b559908f60d12d1d475f95634bcae0c5b9
Todor Mladenov <toshko958@gmail.com>
2024-07-23 15:21:02 +0200

    Move Diary Date as separate component

--- src/components/diary/diary-section/DiarySection.jsx
+++ src/components/diary/diary-section/DiarySection.jsx
@@ -1,16 +1,9 @@
-import { Link } from "react-router-dom";
+import DiaryDate from "./diary-date/DiaryDate";
 
 export default function DiarySection() {
     return (
         <>
-            <section className="date-diary">
-                <h1>Your Food Diary For:</h1>
-                <div className="date">
-                    <Link to="/diary/previousDate">back</Link>
-                    <p>23 July 2024</p>
-                    <Link to="/diary/nextDate">next</Link>
-                </div>
-            </section>
+            <DiaryDate />
 
             <section className="food-diary">
                 <table className="diary">
---
+++ src/components/diary/diary-section/diary-date/DiaryDate.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+export default function DiaryDate() {
+    return (
+        <section className="date-diary">
+            <h1>Your Food Diary For:</h1>
+            <div className="date">
+                <Link to="/diary/previousDate">back</Link>
+                <p>23 July 2024</p>
+                <Link to="/diary/nextDate">next</Link>
+            </div>
+        </section>
+    );
+}
