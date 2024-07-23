290ea9f6eb2a6ad9f94aabed8b61f6dcd19b2634
Todor Mladenov <toshko958@gmail.com>
2024-07-23 14:33:04 +0200

    Add Register component

---
+++ src/components/auth/register/Register.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+
+export default function Register() {
+    return (
+        <form className="form">
+            <h2>Register</h2>
+
+            <div className="field-wrapper">
+                <input type="text" id="username" name="username" placeholder='Username' autoComplete="on" />
+                <label htmlFor="username">Username</label>
+            </div>
+
+            <div className="field-wrapper">
+                <input type="password" id="password" name="password" placeholder='Password' autoComplete="on" />
+                <label htmlFor="password">Password</label>
+            </div>
+
+            <div className="field-wrapper">
+                <input type="password" id="rePassword" name="rePassword" placeholder='Repeat Password' autoComplete="on" />
+                <label htmlFor="rePassword">Repeat Password</label>
+            </div>
+
+            <div className="field-wrapper">
+                <span>Already have account? <Link to="/auth/login">Login here</Link></span>
+            </div>
+
+            <div className="field-wrapper">
+                <input type="submit" value="Register" />
+            </div>
+
+        </form>
+    );
+}
