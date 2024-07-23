e92671b6652b7c7548d4ff933e73270fcc989264
Todor Mladenov <toshko958@gmail.com>
2024-07-23 13:45:23 +0200

    Add style to the Login component

--- src/components/auth/login/Login.jsx
+++ src/components/auth/login/Login.jsx
@@ -1,22 +1,30 @@
-export default function Login(){
-    return(
+import { Link } from 'react-router-dom';
+import './Login.css'
+
+export default function Login() {
+    return (
         <div className="login-section">
             <form className="login-form">
                 <h2>Login</h2>
 
                 <div className="field-wrapper">
-                    <label htmlFor="username"></label>
-                    <input type="text" id="username" name="username"/>
+                    <input type="text" id="username" name="username" placeholder='Username' />
+                    <label htmlFor="username">Username</label>
                 </div>
 
                 <div className="field-wrapper">
-                    <label htmlFor="username"></label>
-                    <input type="text" id="username" name="username"/>
+                    <input type="password" id="password" name="password" placeholder='Password' />
+                    <label htmlFor="password">Password</label>
                 </div>
 
                 <div className="field-wrapper">
-                    <input type="submit" value="Login"/>
+                    <span>You don't have account? <Link to="/auth/register">Register here</Link></span>
                 </div>
+
+                <div className="field-wrapper">
+                    <input type="submit" value="Login" />
+                </div>
+
             </form>
         </div>
     );
