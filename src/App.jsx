8e8c6061f5392ab03406b49e7ca6162a921798c4
Todor Mladenov <toshko958@gmail.com>
2024-07-23 14:34:11 +0200

    Update the style for auth components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Login from './components/auth/login/Login';
+import Register from './components/auth/register/Register';
 import Header from './components/header/Header';
 import { Routes, Route } from 'react-router-dom';
 
@@ -9,9 +10,12 @@ function App() {
     <>
       <Header />
 
-      <Routes>
-        <Route path='/auth/login' element={<Login />} />
-      </Routes>
+      <div className="wrapper">
+        <Routes>
+          <Route path='/auth/login' element={<Login />} />
+          <Route path='/auth/register' element={<Register />} />
+        </Routes>
+      </div>
     </>
   );
 }
--- src/components/auth/login/Login.jsx
+++ src/components/auth/login/Login.jsx
@@ -1,31 +1,29 @@
 import { Link } from 'react-router-dom';
-import './Login.css'
+import '../Auth.css'
 
 export default function Login() {
     return (
-        <div className="login-section">
-            <form className="login-form">
-                <h2>Login</h2>
+        <form className="form">
+            <h2>Login</h2>
 
-                <div className="field-wrapper">
-                    <input type="text" id="username" name="username" placeholder='Username' />
-                    <label htmlFor="username">Username</label>
-                </div>
+            <div className="field-wrapper">
+                <input type="text" id="username" name="username" placeholder='Username' autoComplete='on'/>
+                <label htmlFor="username">Username</label>
+            </div>
 
-                <div className="field-wrapper">
-                    <input type="password" id="password" name="password" placeholder='Password' />
-                    <label htmlFor="password">Password</label>
-                </div>
+            <div className="field-wrapper">
+                <input type="password" id="password" name="password" placeholder='Password' autoComplete='on'/>
+                <label htmlFor="password">Password</label>
+            </div>
 
-                <div className="field-wrapper">
-                    <span>You don't have account? <Link to="/auth/register">Register here</Link></span>
-                </div>
+            <div className="field-wrapper">
+                <span>You don't have account? <Link to="/auth/register">Register here</Link></span>
+            </div>
 
-                <div className="field-wrapper">
-                    <input type="submit" value="Login" />
-                </div>
+            <div className="field-wrapper">
+                <input type="submit" value="Login" />
+            </div>
 
-            </form>
-        </div>
+        </form>
     );
 }
