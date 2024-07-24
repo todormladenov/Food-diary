cfac7a8be65f9c9cf449873786df18088dd9e414
Todor Mladenov <toshko958@gmail.com>
2024-07-24 15:18:23 +0200

    Add data fetching and state management to DiarySection component

--- src/components/diary/diary-section/DiarySection.jsx
+++ src/components/diary/diary-section/DiarySection.jsx
@@ -5,10 +5,19 @@ import DiaryBreakfast from "./diary-breakfast/DiaryBreakfast";
 import DiaryLunch from "./diary-lunch/DiaryLunch";
 import DiaryDinner from "./diary-dinner/DiaryDinner";
 import DiarySnack from "./diary-snack/DiarySnack";
+import { useEffect, useState } from "react";
+import { getOneDate } from "../../../services/dateAPI";
 
 export default function DiarySection() {
+    const [diary, setDiary] = useState({});
     const { diaryDate } = useParams();
 
+    useEffect(() => {
+        (async () => {
+            const result = await getOneDate('PCwMKhyzR1', diaryDate);
+            setDiary(result.results[0])
+        })()
+    }, [])
     return (
         <>
             <DiaryDate diaryDate={diaryDate} />
@@ -25,10 +34,10 @@ export default function DiarySection() {
                     </colgroup>
                     <tbody>
 
-                        <DiaryBreakfast />
-                        <DiaryLunch />
-                        <DiaryDinner />
-                        <DiarySnack />
+                        <DiaryBreakfast breakfast={diary.breakfast || []} />
+                        <DiaryLunch lunch={diary.lunch || []} />
+                        <DiaryDinner dinner={diary.dinner || []} />
+                        <DiarySnack snack={diary.snack || []} />
 
                     </tbody>
                 </table>
