22623df2d6061ca5d0316b1116c12f81770ef7c2
Todor Mladenov <toshko958@gmail.com>
2024-07-26 15:57:57 +0200

    Add NutritionInfo component and styles

---
+++ src/components/add-food/nutrition-info/NutritionInfo.jsx
@@ -0,0 +1,27 @@
+import './NutritionInfo.css'
+
+export default function NutritionInfo({ food, onHideNutritionInfo }) {
+    return (
+        <div className="overlay">
+            <div className="backdrop" onClick={onHideNutritionInfo}></div>
+            <div className="modal">
+                <div className="content">
+                    <header className="headers">
+                        <h2>Nutrition Information</h2>
+                        <button className="close-btn" onClick={onHideNutritionInfo}>
+                            <i className="fa-solid fa-x"></i>
+                        </button>
+                    </header>
+                    <div className="nutrition-info">
+                        <p>Food <strong>{food.name}</strong></p>
+                        <p>Serving Size <strong>{food.servingSize}</strong></p>
+                        <p>Calories <strong>{food.calories}</strong></p>
+                        <p>Carbs <strong>{food.carbs}</strong></p>
+                        <p>Fats <strong>{food.fat}</strong></p>
+                        <p>Protein <strong>{food.protein}</strong></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
