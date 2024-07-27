5b06396d2b9882c89bc259557413c28a715ae8a6
Todor Mladenov <toshko958@gmail.com>
2024-07-27 10:58:18 +0200

    Add getOneDiaryDateById and UpdateDiaryDateById

--- src/services/dateAPI.js
+++ src/services/dateAPI.js
@@ -1,9 +1,9 @@
 import createPointer from "../utils/createPointer";
-import { get, post } from "./api";
+import { get, post, put } from "./api";
 
 const baseUrl = 'https://parseapi.back4app.com/classes/Date';
 
-export const getOneDate = async (userId, diaryDate) => {
+export const getOneDiaryDate = async (userId, diaryDate) => {
     const constraints = {
         dateString: diaryDate,
         userId: createPointer('_User', userId),
@@ -23,3 +23,7 @@ export const createDiaryDate = async ({ userId, diaryDate }) => {
     const body = { userId: pointer, dateString: diaryDate };
     return post(baseUrl, body);
 }
+
+export const getOneDiaryDateById = (dateId) => get(`${baseUrl}/${dateId}`);
+
+export const updateDiaryDateById = (dateId, diaryData) => put(`${baseUrl}/${dateId}`, diaryData);
