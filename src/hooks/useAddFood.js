2de7aab289b879a5c1023186a9f1e925ae594926
Todor Mladenov <toshko958@gmail.com>
2024-07-27 10:57:31 +0200

    Add useAddFood hook

---
+++ src/hooks/useAddFood.js
@@ -0,0 +1,33 @@
+import { getOneDiaryDateById, updateDiaryDateById } from "../services/dateAPI";
+import { calculateTotalFoodMacros } from "../utils/calculateMacros";
+
+export const useAddFood = (food, dateId, mealType) => {
+    const initialFoodValues = {
+        objectId: food.objectId,
+        protein: food.protein,
+        name: food.name,
+        calories: food.calories,
+        carbs: food.carbs,
+        fat: food.fat,
+        servings: 1
+    };
+
+    const addFood = async (foodValues) => {
+        const diary = await getOneDiaryDateById(dateId);
+
+        if (!diary[mealType]) {
+            diary[mealType] = [];
+        }
+
+        const calculatedFoodMacros = calculateTotalFoodMacros(foodValues);
+
+        diary[mealType] = [...diary[mealType], calculatedFoodMacros];
+
+        return await updateDiaryDateById(dateId, { [mealType]: diary[mealType] });
+    }
+
+    return {
+        initialFoodValues,
+        addFood
+    }
+}
