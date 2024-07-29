32fdfd3ef6c9043a4a858ff82b2c94f944e4d309
Todor Mladenov <toshko958@gmail.com>
2024-07-29 22:39:21 +0200

    Add Snackbar component

---
+++ src/components/snackbar/Snackbar.jsx
@@ -0,0 +1,10 @@
+import './Snackbar.css'
+
+export default function Snackbar() {
+
+    return (
+        <div>
+            <div className="snackbar show">Error Here</div>
+        </div>
+    );
+}
