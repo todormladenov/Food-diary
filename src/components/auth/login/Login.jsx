114e5de490d59a93ad2306dd53d9a746a72c4d75
Todor Mladenov <toshko958@gmail.com>
2024-08-01 11:19:35 +0200

    Update error handling

--- src/components/auth/login/Login.jsx
+++ src/components/auth/login/Login.jsx
@@ -2,7 +2,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import '../Auth.css';
 import { login } from '../../../services/authAPI';
 import { useForm } from '../../../hooks/useForm';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AuthContext } from '../../../contexts/AuthContext';
 import { SnackbarContext } from '../../../contexts/SnackbarContext';
 
@@ -12,11 +12,17 @@ const initialValues = {
 }
 
 export default function Login() {
+    const [errors, setErrors] = useState({ message: '' });
+
     const authState = useContext(AuthContext);
     const snackbar = useContext(SnackbarContext);
     const navigator = useNavigate();
 
     const loginHandler = async (values) => {
+        if (values.username.trim() === '' || values.password.trim() === '') {
+            return setErrors({ message: 'Username and password is required' });
+        }
+
         try {
             const user = await login(values.username, values.password);
             authState.changeAuthState(user);
@@ -30,12 +36,11 @@ export default function Login() {
     return (
         <form className="form" onSubmit={submitHandler}>
             <h2>Login</h2>
-
+            {errors.message && <p className='error'>{errors.message}</p>}
             <div className="field-wrapper">
                 <input type="text" id="username" name="username" placeholder='Username' autoComplete='on'
                     value={formValues.username}
                     onChange={changeHandler}
-                    required
                 />
                 <label htmlFor="username">Username</label>
             </div>
@@ -44,7 +49,6 @@ export default function Login() {
                 <input type="password" id="password" name="password" placeholder='Password' autoComplete='on'
                     value={formValues.password}
                     onChange={changeHandler}
-                    required
                 />
                 <label htmlFor="password">Password</label>
             </div>
