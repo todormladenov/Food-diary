9951899397f2dfa77457c1e479ad22b524dd9c01
Todor Mladenov <toshko958@gmail.com>
2024-08-02 11:12:04 +0200

    Add FoodItem component

---
+++ src/components/food/my-created-foods/food-item/FoodItem.jsx
@@ -0,0 +1,20 @@
+export default function FoodItem({ food }) {
+    return (
+        <div className="food-card">
+            <h2>{food.name}</h2>
+            <p><span>Calories:</span> {food.calories}</p>
+            <p><span>Protein:</span> {food.protein}</p>
+            <p><span>Carbs:</span> {food.carbs}</p>
+            <p><span>Fat:</span> {food.fat}</p>
+            <p><span>Serving Size:</span> {food.servingSize}</p>
+            <div className="food-buttons">
+                <button className="edit-button" >
+                    <i className="fa-solid fa-pen-to-square"></i>
+                </button>
+                <button className="delete-button" >
+                    <i className="fa-solid fa-trash"></i>
+                </button>
+            </div>
+        </div>
+    );
+}
