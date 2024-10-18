c4d22abad9795ad0651cfd6c1ff1184551fc4edc
Todor Mladenov <toshko958@gmail.com>
2024-10-18 11:12:31 +0200

    Update onChange functionality

--- src/components/shared/category-selector/CategorySelector.jsx
+++ src/components/shared/category-selector/CategorySelector.jsx
@@ -1,8 +1,8 @@
 import './CategorySelector.css';
 import { useState } from "react";
 
-export default function CategorySelector({ onSearch }) {
-    const [selectedCategory, setSelectedCategory] = useState("");
+export default function CategorySelector({ onChange, value }) {
+    const [selectedCategory, setSelectedCategory] = useState(value);
 
     const handleCategoryChange = (e) => {
         const category = e.target.value;
@@ -11,7 +11,7 @@ export default function CategorySelector({ onSearch }) {
         if (!category) {
             return
         }
-        onSearch(category)
+        onChange(e);
     }
 
     return (
@@ -20,43 +20,43 @@ export default function CategorySelector({ onSearch }) {
             <select
                 className="category-selector"
                 name="category"
-                value={selectedCategory}
+                value={value = !undefined ? value : selectedCategory}
                 onChange={handleCategoryChange}
             >
 
-                <option value="">Избери категория</option>
-                <option value="Солени пакетирани храни ">Солени пакетирени храни</option>
-                <option value="Яйца">Яйца</option>
-                <option value="Алкохолни напитки">Алкохолни напитки</option>
-                <option value="Безалкохолни напитки">Безалкохолни напитки</option>
-                <option value="Бобови">Бобови</option>
-                <option value="Веган">Веган</option>
-                <option value="Въглехидрати">Въглехидрати</option>
-                <option value="Готвени ястия (домашна храна/ресторант/закусвалня)">Готвени ястия (домашна храна/ресторант/закусвалня)</option>
-                <option value="Десерти">Десерти</option>
-                <option value="Животинска карантия (месо)">Животинска карантия (месо)</option>
-                <option value="Закуски">Закуски</option>
-                <option value="Замразени храни">Замразени храни</option>
-                <option value="Зеленчуци">Зеленчуци</option>
-                <option value="Мед/сладка/конфитюри">Мед/сладка/конфитюри</option>
-                <option value="Месо">Месо</option>
-                <option value="Млечни">Млечни</option>
-                <option value="Олиа/масла">Олиа/масла</option>
-                <option value="Плодове">Плодове</option>
-                <option value="Подправки">Подправки</option>
-                <option value="Протеин на прах">Протеин на прах</option>
-                <option value="Протеинови барове">Протеинови барове</option>
-                <option value="Растителни алтернативи на млечни продукти">Растителни алтернативи на млечни продукти</option>
-                <option value="Риба">Риба</option>
-                <option value="Сладки пакетирани храни">Сладки пакетирани храни</option>
-                <option value="Сосове">Сосове</option>
-                <option value="Супи">Супи</option>
-                <option value="Сушени плодове">Сушени плодове</option>
-                <option value="Ядки/семена">Ядки/семена</option>
-                <option value="Ядкови масла">Ядкови масла</option>
+                <option value="">Choose a category</option>
+                <option value="Солени пакетирани храни ">Salty packaged foods</option>
+                <option value="Яйца">Eggs</option>
+                <option value="Алкохолни напитки">Alcoholic beverages</option>
+                <option value="Безалкохолни напитки">Soft drinks</option>
+                <option value="Бобови">Legumes (grains)</option>
+                <option value="Веган">Vegan</option>
+                <option value="Въглехидрати">Carbohydrates</option>
+                <option value="Готвени ястия (домашна храна/ресторант/закусвалня)">Cooked meals (home cooked food/restaurant/diner)</option>
+                <option value="Десерти">Desserts</option>
+                <option value="Животинска карантия (месо)">Animal offal (meat)</option>
+                <option value="Закуски">Breakfast meals</option>
+                <option value="Замразени храни">Frozen foods</option>
+                <option value="Зеленчуци">Vegetables</option>
+                <option value="Мед/сладка/конфитюри">Honey/sweet/jams</option>
+                <option value="Месо">Meat</option>
+                <option value="Млечни">Dairy</option>
+                <option value="Олиа/масла">Oils/butters</option>
+                <option value="Плодове">Fruits</option>
+                <option value="Подправки">Spices</option>
+                <option value="Протеин на прах">Protein powder</option>
+                <option value="Протеинови барове">Protein bars</option>
+                <option value="Растителни алтернативи на млечни продукти">Plant-based alternatives to dairy products</option>
+                <option value="Риба">Fish</option>
+                <option value="Сладки пакетирани храни">Sweet packaged foods</option>
+                <option value="Сосове">Sauces</option>
+                <option value="Супи">Soups</option>
+                <option value="Сушени плодове">Dried fruits</option>
+                <option value="Ядки/семена">Nuts/seeds</option>
+                <option value="Ядкови масла">Nut butter</option>
                 <option value="Fast food">Fast food</option>
-                <option value="Shape Up Kitchen рецепти">Shape Up Kitchen рецепти</option>
-                <option value="Други">Други</option>
+                <option value="Shape Up Kitchen рецепти">Shape Up Kitchen recipes</option>
+                <option value="Други">Others</option>
             </select>
         </>
     );
